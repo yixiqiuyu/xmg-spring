dfcea9caf1cbda912ae129a9a412691839c8bf02
yixiqiuyu <2419230752@qq.com>
2022-03-29 20:00:07 +0800

    @Autowired 和 @Inject原理

---
+++ dependency-injection/src/main/java/com/yixiqiuyu/dependency/injection/AnnotationDependencyInjectionResolutionDemo.java
@@ -0,0 +1,54 @@
+package com.yixiqiuyu.dependency.injection;
+
+import com.yixiqiuyu.spring.overview.domain.User;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.inject.Inject;
+import java.util.Set;
+
+/**
+ * @Description: 注解驱动的依赖注入处理过程
+ * @Author: yixiqiuyu
+ * @Date: 2022/3/15  22:58
+ */
+
+public class AnnotationDependencyInjectionResolutionDemo {
+
+    @Autowired
+    private User user;  // DependencyDescriptor ->
+                        // 必须(required = true)
+                        // 实时注入 (eager = true)
+                        // 通过类型(User.class)
+                        // 字段名称("user")
+                        // 是否首要(primary = true)
+    @Inject
+    private User injectedUser;
+
+
+    public static void main(String[] args) {
+        // 创建BeanFactory容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+        applicationContext.register(AnnotationDependencyInjectionResolutionDemo.class);
+
+        XmlBeanDefinitionReader definitionReader = new XmlBeanDefinitionReader(applicationContext);
+        String resourcePath = "classpath:/META-INF/dependency-lookup.xml";
+        // 加载XML资源，解析并生成 BeanDefinition
+        definitionReader.loadBeanDefinitions(resourcePath);
+
+        applicationContext.refresh();
+
+        AnnotationDependencyInjectionResolutionDemo demo = applicationContext.getBean(AnnotationDependencyInjectionResolutionDemo.class);
+
+        // 期待输出 superUser Bean
+        System.out.println("demo.user:" + demo.user);
+
+        System.out.println("demo.injectedUser:" + demo.injectedUser);
+
+        applicationContext.close();
+    }
+}
